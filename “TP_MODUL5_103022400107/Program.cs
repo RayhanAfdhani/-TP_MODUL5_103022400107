c56719bdc240753c51721e0e828fb40199ae2359
Rayhan <147673112+RayhanAfdhani@users.noreply.github.com>
2026-03-29 23:01:34 +0700

    commit message

--- “TP_MODUL5_103022400107/Program.cs
+++ “TP_MODUL5_103022400107/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+public class DataGeneric<T>
+{
+    private T data;
+    public DataGeneric(T data)
+    {
+        this.data = data;
+    }
+    public void PrintData()
+    {
+        Console.WriteLine("Data yang tersimpan adalah: " + this.data);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DataGeneric<string> dataNIM = new DataGeneric<string>("103022400107");
+        dataNIM.PrintData();
+    }
+}
